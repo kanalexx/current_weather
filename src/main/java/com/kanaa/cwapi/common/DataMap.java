7e558956b654e0c20636273ed6ea923dbff11491
kanaa <A.Kanunnikov@bssys.com>
2019-11-08 20:27:56 +0300

    - Дорабтка мапера.

--- src/main/java/com/kanaa/cwapi/common/DataMap.java
+++ src/main/java/com/kanaa/cwapi/common/DataMap.java
@@ -1,23 +1,34 @@
 package com.kanaa.cwapi.common;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DataMap {
 
   private Class dataObjectClass;
-  private List<FieldMap> fields = new ArrayList<>();
+  /** Связь "имя поля" - "поле объекта" */
+  private HashMap<String, Field> classFieldMap = new HashMap<>();
+  /** Связь "имя колонки" - "поле объекта" */
+  private HashMap<String, FieldMap> fields = new HashMap<>();
 
   public DataMap(Class dataObjectClass) {
     this.dataObjectClass = dataObjectClass;
+    // сканирование всех полей по всей иерархии наследования
+    for(Class aClass = dataObjectClass; aClass != null; aClass = aClass.getSuperclass()) {
+      for(Field field : aClass.getDeclaredFields()) {
+        classFieldMap.put(field.getName(), field);
+      }
+    }
   }
 
   public void addField(String columnName, String fieldName) throws UserException {
     try {
-      Field field = dataObjectClass.getDeclaredField(fieldName);
+      Field field = classFieldMap.get(fieldName);
       field.setAccessible(true);
-      fields.add(createFieldMap(columnName, field));
+      fields.put(columnName, createFieldMap(columnName, field));
     } catch (Exception e) {
       throw new UserException("unable to set up field: " + fieldName, e);
     }
@@ -25,5 +36,16 @@ public class DataMap {
 
   private FieldMap createFieldMap(String columnName, Field field) {
     return new FieldMap(columnName, field);
+  }
+
+  public void setField(DataObject dataObject, String columnName, Object columnValue) throws UserException {
+    FieldMap fieldMap = fields.get(columnName);
+    Field field = fieldMap.getField();
+    try {
+      Object value = field.getType().getConstructor(columnValue.getClass()).newInstance(columnValue);
+      field.set(dataObject, value);
+    } catch (Exception e) {
+      throw new UserException("Error in setting " + fieldMap.getField().getName(), e);
+    }
   }
 }
--- src/test/groovy/com/kanaa/cwapi/common/DataMapTest.groovy
+++ src/test/groovy/com/kanaa/cwapi/common/DataMapTest.groovy
@@ -3,18 +3,51 @@ package com.kanaa.cwapi.common
 import org.junit.Before
 import org.junit.Test
 
+import static org.mockito.Mockito.mock
+
 class DataMapTest extends MyTest {
 
+  class MockDataObject extends DataObject {
+
+    BigDecimal amount = new BigDecimal(0);
+
+    MockDataObject(Context ctx) throws UserException {
+      super(ctx)
+    }
+
+    @Override
+    protected DataMapper getDataMapper() throws UserException {
+      return mock(DataMapper)
+    }
+
+    BigDecimal getAmount() {
+      return amount;
+    }
+  }
+
+  static storage = [
+      "ID" : "555",
+      "AMOUNT" : "123.45"
+  ];
+
   DataMap dataMap;
+  MockDataObject dataObject;
 
   @Before
   void setUp() {
-    dataMap = new DataMap(Class.forName("com.kanaa.cwapi.common.DataObject"))
+    dataObject = new MockDataObject(mock(Context.class))
+    dataMap = new DataMap(dataObject.getClass())
   }
 
   @Test
   void testAddField() {
-    dataMap.addField("ID", "id");
-
+    dataMap.addField("ID", "id")
+    dataMap.addField("AMOUNT", "amount")
+    storage.each {it ->
+      dataMap.setField(dataObject, it.key, it.value)
+    }
+    assert dataObject.getId()
+    assert 555L == dataObject.getId()
+    assert 123.45 == dataObject.getAmount()
   }
 }
